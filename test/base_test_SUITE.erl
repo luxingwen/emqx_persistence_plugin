551c5bc266bfa4932bc37183be44d3d9da7b9c86
wwhai <751957846@qq.com>
2021-02-04 17:35:00 +0800

    add test cases

--- test/base_test_SUITE.erl
+++ test/base_test_SUITE.erl
@@ -37,12 +37,31 @@ t_client_id_test(_) ->
     emqtt:disconnect(C1),
     emqtt:disconnect(C2).
 
+t_p2p_test(_) ->
+    {ok, C1} = emqtt:start_link([{host, "127.0.0.1"} ,
+                                 {clientid, <<"clientid1">>}]),
+    {ok, _} = emqtt:connect(C1),
+    {ok, C2} = emqtt:start_link([{host, "127.0.0.1"} ,
+                                 {clientid, <<"clientid2">>}]),
+    {ok, _} = emqtt:connect(C2),
+    emqtt:publish(C1, <<"$P2P/clientid2">>, <<"ok">>, qos2),
+
+    receive
+        {publish, #message{topic = <<"$P2P/clientid2">>, payload = Payload2}} ->
+            <<"ok">> = Payload2;
+        {puback, _ } ->
+            ok;
+        Other2 ->
+            ct:print("Other Msg >=> :~p~n", [Other2])
+    end,
+    emqtt:disconnect(C1),
+    emqtt:disconnect(C2).
+
 t_save_to_mysql(_) ->
     {ok, C1} = emqtt:start_link([{host, "127.0.0.1"} ,
                                  {clientid, <<"clientid1">>},
                                  {username, <<"username">>},
                                  {password, <<"password">>}]),
     {ok, _} = emqtt:connect(C1),
-    emqtt:publish(C1, <<"$PERSISTENCE/test">>, <<"ok">>, qos2),
-    emqtt:disconnect(C1),
-    ok.
+    {ok, 2} = emqtt:publish(C1, <<"$PERSISTENCE/test">>, <<"ok">>, qos2),
+    ok = emqtt:disconnect(C1).
