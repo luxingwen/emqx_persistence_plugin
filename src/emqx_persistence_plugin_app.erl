bcde5721e5be7df716ef31bb0c01926b40f271c8
wwhai <751957846@qq.com>
2021-02-05 22:53:07 +0800

    fix bug

--- src/emqx_persistence_plugin_app.erl
+++ src/emqx_persistence_plugin_app.erl
@@ -19,10 +19,8 @@ start(_StartType, _StartArgs) ->
     {ok, Sup} = emqx_persistence_plugin_sup:start_link(),
     ?APP:load(),
     ?APP:register_metrics(),
-    emqx:hook('client.authenticate', fun emqx_persistence_plugin:on_client_authenticate/2, []),
     {ok, Sup}.
 
 stop(_State) ->
     ?APP:unload(),
-    emqx:unhook('client.authenticate', fun emqx_persistence_plugin:on_client_authenticate/2),
     ok.
